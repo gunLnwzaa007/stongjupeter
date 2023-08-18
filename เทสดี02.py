90cc4d463fcdc7637797c096e312ee7afedd0d74
gunLnwzaa007 <gunnockba@gmail.com>
2023-08-18 11:15:50 +0700

    code v.1 creat 3 flie na kub

---
+++ เทสดี02.py
@@ -0,0 +1,2 @@
+print('hello',end='')
+print('hinaja',end="")
---
+++ เทสดี03.py
@@ -0,0 +1,11 @@
+print('AAA'); print('BBB')
+print("ccc")
+print('DDD\nEEE\nFFF')
+print('DDD\tEEE\tFFF')
+print('I miss her "i miss him"')
+print('abc\\def')
+print('''
+      ya..
+      ha..
+      ma..
+      ''')
